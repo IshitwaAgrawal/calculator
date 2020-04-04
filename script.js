6af8749614f66c33f57fc35181d458a78a475d5c
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-04-04 19:58:01 +0530

    updated

--- script.js
+++ script.js
@@ -4,8 +4,33 @@ const operators = document.querySelectorAll(".operator");
 const allClear = document.querySelector('.all-clear');
 const equalBtn = document.querySelector('.equal-sign');
 const percentageBtn = document.querySelector('.percentage');
+const clear = document.querySelector('.clear');
 
 let expr = '';
+const reset = () =>{
+    calciScreen.value = "0";
+    expr = "";
+};
+window.onload = reset;
+
+clear.addEventListener('click',() => {
+    let t = calciScreen.value.toString();
+    console.log(t.indexOf('%'));
+    //if(((t.charAt(t.length-1))===("%"))){
+    if(t.indexOf("%")!=-1){
+        expr = expr.substring(0,expr.length-1);
+        let r = eval(expr);
+        r = r*100;
+        expr = ""+r;
+        console.log('inside');
+        t = t.substring(0,t.length-1);
+        calciScreen.value = t;
+    }
+    else{
+        expr = expr.substring(0,expr.length-1);
+        calciScreen.value = expr;
+    }
+});
 
 equalBtn.addEventListener('click', () =>{
     let res = eval(expr);
@@ -13,8 +38,7 @@ equalBtn.addEventListener('click', () =>{
 });
 
 allClear.addEventListener('click',() => {
-    calciScreen.value = "";
-    expr = '';
+    reset();
 });
 percentageBtn.addEventListener('click',() => {
     let r = eval(expr);
@@ -25,6 +49,9 @@ percentageBtn.addEventListener('click',() => {
 
 });
 const updateScreen = (number) =>{
+    if(calciScreen.value == "0"){
+        calciScreen.value = "";
+    }
     expr += number;
     calciScreen.value += number;
 };
